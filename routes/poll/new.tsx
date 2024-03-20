0d655fb95064ee43a91713295363bb1ad2d65102
Cameron Leslie <cameronleslie@gmail.com>
2024-03-20 08:39:43 -0700

    setting up form for creating poll, and response

--- routes/poll/new.tsx
+++ routes/poll/new.tsx
@@ -1,10 +1,38 @@
-import { RouteContext } from "$fresh/server.ts";
+import { Handlers } from "$fresh/server.ts";
+import { osType } from "$std/path/_os.ts";
 
-export default function Share() {
+export const handler: Handlers = {
+  async GET(req, ctx) {
+    return await ctx.render();
+  },
+  async POST(req, ctx) {
+    const form = await req.formData();
+    const options = form.get("pollOptions")?.toString().split(/\r?\n/);
+    const id = form.get("id")?.toString();
+    console.log(options); 
+    // write to db?
+    
+
+    // Redirect user to thank you page.
+    const headers = new Headers();
+    headers.set("location", `${id}/share`);
+    return new Response(null, {
+      status: 303,
+      headers,
+    });
+  },
+};
+
+export default function Subscribe() {
+  const id = btoa(Date.now().toString())
   return (
-    <div>
-      This is where we create a new poll. after we submit the poll form, we
-      redirect to, poll/[id]/share, the server should return the new polls id
-    </div>
+    <>
+      <form method="post">
+        <input type="hidden" name="id" value={id}></input>
+        <textarea type="text" name="pollOptions"/><br/>
+        <button type="submit">Make Poll</button>
+      </form>
+    </>
   );
 }
+
