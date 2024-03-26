e5366cc97920f64c73269f1d8265fea32d35a9be
Paul Salaets <psalaets@gmail.com>
2024-03-25 23:11:19 -0500

    Fix typo

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -6,7 +6,7 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Hyōketsu 票決 - Ephmeral Voting</title>
+        <title>Hyōketsu 票決 - Ephemeral Voting</title>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true"/>
         <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1:wght@100..900&display=swap" rel="stylesheet"></link>
