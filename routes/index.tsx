00bdc2be7d2e76549c5970cc8a09470087e46b1c
Paul Salaets <psalaets@gmail.com>
2024-03-25 22:24:07 -0500

    Fix typo

--- routes/index.tsx
+++ routes/index.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div>
       <h1 className={`h2`}>Hyōketsu <br/><b className={`h1`}>票決</b></h1>
-      <p>Ephmerial voting made easy.</p>
+      <p>Ephemeral voting made easy.</p>
       <a href="/poll/new" className="c-button c-button--primary">Make a New Poll</a>
     </div>
   );
