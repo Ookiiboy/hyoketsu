22342181d9e1bbfdbb5c66346b026e17d89afd8e
Paul Salaets <psalaets@gmail.com>
2024-03-28 13:50:43 -0500

    Enable cron and schedule expiration job

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,3 +1,12 @@
 import { defineConfig } from "$fresh/server.ts";
+import { deleteExpiredPolls } from './lib/expiration/delete-expired-polls.ts';
+
+Deno.cron('Delete expired polls', {
+  minute: {
+    every: 1
+  }
+}, () => {
+  deleteExpiredPolls();
+});
 
 export default defineConfig({});
