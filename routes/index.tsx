52e4956f953999f941e32ed13c2ba9b0ab184271
Cameron Leslie <cameronleslie@gmail.com>
2024-03-28 09:27:40 -0700

    images, and css

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,7 +1,9 @@
+const Check = () => <span style={{color: "var(--primary-accent-color)"}}>✔</span>
+
 export default function Home() {
   return (
     <>
-      <h1 className={`h2`}>Hyōketsu <br/><b className={`h1`}>票決</b></h1>
+      <h1 className={`h2`}>Hyōketsu <Check/><br/><b className={`h1`}>票決</b></h1>
       <p>Ephemeral voting made easy.</p>
       <a href="/poll/new" className="c-button c-button--primary">Make a New Poll</a>
     </>
