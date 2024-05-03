242bc62f35e99cfee2d7b694534394da9a10728c
Paul Salaets <psalaets@gmail.com>
2024-05-03 00:38:07 -0500

    Refresh when becomes visible but don't auto poll

---
+++ lib/api-client.ts
@@ -0,0 +1,7 @@
+import { Poll } from '../lib/poll.ts';
+
+export function loadPoll(id: Poll['id']) {
+  return fetch(`/api/poll/${id}`)
+    .then(resp => resp.json())
+    .then(json => json as Poll);
+}
