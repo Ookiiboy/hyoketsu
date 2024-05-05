3435387b7a85eeb95b817529aa16900d215b94a6
Paul Salaets <psalaets@gmail.com>
2024-05-05 00:24:10 -0500

    clean up

--- lib/logger.ts
+++ lib/logger.ts
@@ -1,7 +1,7 @@
 import * as log from 'log';
 
 const minLogLevel: log.LevelName = 'INFO';
-console.log('configuring logger')
+
 log.setup({
   handlers: {
     default: new log.handlers.ConsoleHandler(minLogLevel, {
