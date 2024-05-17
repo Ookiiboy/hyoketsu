6557964111f7d83f3f635191842c13d06e61b3b3
Cameron Leslie <cameronleslie@gmail.com>
2024-05-17 14:52:49 -0700

    swapping out lefthook for nix-githooks

--- flake.nix
+++ flake.nix
@@ -2,47 +2,51 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     systems.url = "github:nix-systems/default";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
   };
 
   outputs = {
+    self,
     systems,
     nixpkgs,
+    pre-commit-hooks,
     ...
   } @ inputs: let
-    eachSystem = f:
-      nixpkgs.lib.genAttrs (import systems) (
-        system:
-          f nixpkgs.legacyPackages.${system}
-      );
+    forAllSystems = nixpkgs.lib.genAttrs (import systems);
   in {
-    formatter = {
-      "aarch64-darwin" = nixpkgs.legacyPackages.aarch64-darwin.alejandra;
-      "x86_64-linux" = nixpkgs.legacyPackages.x86_64-linux.alejandra;
-    };
-    devShells = eachSystem (pkgs: {
-      default = pkgs.mkShell {
+    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
+    checks = forAllSystems (system: {
+      pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
+        src = ./.;
+        hooks = {
+          alejandra.enable = true;
+          deadnix.enable = true;
+          statix.enable = true;
+          flake-checker.enable = true;
+          checkmake.enable = true;
+          denofmt.enable = true;
+          denolint.enable = true;
+          shellcheck.enable = true;
+          check-json.enable = true;
+        };
+      };
+    });
+    devShells = forAllSystems (system: {
+      default = nixpkgs.legacyPackages.${system}.mkShell {
         shellHook = ''
+          ${self.checks.${system}.pre-commit-check.shellHook}
           export PATH="bin:$PATH"
         '';
         SHELL_ENV = "dev";
         PORT = 6969;
-        buildInputs = with pkgs; [
-          lefthook
-          shellcheck
-          gnumake
-          checkmake
-          statix
-          deadnix
-          deno
-          # Typescript LSP for SublimeText runs on node
-          nodejs_20
-        ];
-      };
-      production = pkgs.mkShell {
-        SHELL_ENV = "prod";
-        buildInputs = with pkgs; [
-          deno
-        ];
+        buildInputs = with nixpkgs.legacyPackages.${system};
+          [
+            gnumake
+            deno
+            # Typescript LSP for SublimeText runs on node
+            nodejs_20
+          ]
+          ++ self.checks.${system}.pre-commit-check.enabledPackages;
       };
     });
   };
